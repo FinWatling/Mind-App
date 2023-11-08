71ff42a30fbdf817d225e822b58525e717f1584a
finwatling <finlaywatling@gmail.com>
2023-11-08 19:45:13 +0000

    Updated radio button styling and added logo

--- mind-app/src/MainHero.js
+++ mind-app/src/MainHero.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import logo from "./img/brainlogo.png"
 
 export default class MainHero extends Component {
   render() {
     return (
-      <div className="p-3">
-      <h1 className="title-text">Mind App</h1>
+      <div className="hero"> 
+      <h1 className='inline'>Mind App</h1> <img className="inline-block p-4" src={logo} alt="Logo"/>
       </div>
     )
   }
