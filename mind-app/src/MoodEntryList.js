972483207fdf92e8ad765f05858f3d7ce65574f1
Fin Watling <finlaywatling@gmail.com>
2023-11-09 19:33:06 +0000

    MoodEntryList.js

---
+++ mind-app/src/MoodEntryList.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class MoodEntryList extends Component {
+  render() {
+    return <div>MoodEntryList</div>;
+  }
+}
+
+export default MoodEntryList;
