8c0f89fa9382ed1b2e4b25150ff111a2f151d5b1
Fin Watling <finlaywatling@gmail.com>
2023-11-09 19:29:07 +0000

    Created date time functionality

--- mind-app/src/MoodRating.js
+++ mind-app/src/MoodRating.js
@@ -2,17 +2,42 @@ import React from "react";
 import { useState } from "react";
 
 export const MoodRating = () => {
-  const [moodSelection, setMoodSelection] = useState("1");
+  const [moodSelection, setMoodSelection] = useState(1);
+  const [moodNotes, setMoodNotes] = useState("");
+  const [moodEntry, setMoodEntry] = useState(null);
 
   const onMoodChange = (e) => {
     setMoodSelection(e.target.value);
-    console.log(e.target.value);
+  };
+
+  const onMoodNotesChange = (e) => {
+    setMoodNotes(e.target.value);
+  };
+
+  function getCurrentDateTime() {
+    const dateTime = new Date();
+    let time = dateTime.getHours() + ":" + dateTime.getMinutes();
+    let date =
+      dateTime.getDate() +
+      "/" +
+      dateTime.getMonth() +
+      "/" +
+      dateTime.getFullYear();
+    return date + " " + time;
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(getCurrentDateTime());
+    console.log(moodNotes);
+    console.log(moodSelection);
   };
 
   return (
     <div className=" pb-4 max-w-screen text-center content-center bg-slate-300 wp">
       <form>
-        <h1 className="text-4xl drop-shadow-sm">Mood rating for today:</h1>
+        <h1 className="text-4xl drop-shadow-sm pb-4">Mood rating for today:</h1>
         <input
           onChange={onMoodChange}
           className="radio-buttons"
@@ -123,13 +148,18 @@ export const MoodRating = () => {
         <label className="radio-buttons" htmlFor="10">
           10
         </label>
-        <h1>Mood comment for today:</h1>
+        <h1 className="text-4xl drop-shadow-sm pb-4">
+          Mood comment for today:
+        </h1>
         <textarea
-          className="border-solid border-2 border-black "
+          onChange={onMoodNotesChange}
+          className="border-solid border-2 border-black w-1/2 h-auto"
           placeholder="How are you feeling?"
         ></textarea>
         <br></br>
-        <button className="submit-button">Submit</button>
+        <button onClick={onSubmit} className="submit-button">
+          Submit
+        </button>
       </form>
     </div>
   );
