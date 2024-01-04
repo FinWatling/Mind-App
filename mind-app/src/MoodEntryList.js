e550bdaa5c3b8d842f829d01f3fe7e029d7e5799
Fin Watling <finlaywatling@gmail.com>
2024-01-04 19:32:15 +0000

    Navigator.share changes

--- mind-app/src/MoodEntryList.js
+++ mind-app/src/MoodEntryList.js
@@ -4,7 +4,7 @@ const { decrypt } = Encryption();
 
 const MoodEntryList = (props) => {
   const [dataMap, setDataMap] = useState(new Map());
-  
+
   useEffect(() => {
     const jsonData = localStorage.getItem("MoodEntries");
 
@@ -22,52 +22,64 @@ const MoodEntryList = (props) => {
   function onShare(value) {
     const encryptedMoodRating = value.moodrating;
     const encryptedMoodNotes = value.moodnotes;
-  
+
     const shareData = {
       title: "Mood Entry",
-      text: `DateTime Info: ${value.datetime} \nEncrypted Mood Rating: ${encryptedMoodRating}\nEncrypted Mood Notes: ${encryptedMoodNotes}\nTemperature: ${Math.round(value.currentweather.currenttemp)}°C\nDescription: ${value.currentweather.currentWeatherDescription}`,
+      text: `DateTime Info: ${
+        value.datetime
+      } \nEncrypted Mood Rating: ${encryptedMoodRating}\nEncrypted Mood Notes: ${encryptedMoodNotes}\nTemperature: ${Math.round(
+        value.currentweather.currenttemp
+      )}°C\nDescription: ${value.currentweather.currentWeatherDescription}`,
     };
-  
-    navigator.share(shareData)
-      .then(() => console.log("Shared successfully"))
-      .catch((error) => console.error("Error sharing:", error));
+
+    if (navigator.share) {
+      navigator
+        .share(shareData)
+        .then(() => console.log("Shared successfully"))
+        .catch((error) => console.error("Error sharing:", error));
+    } else {
+      // Handle the case where the Web Share API is not supported
+      console.error("Web Share API is not supported by this browser");
+    }
   }
 
   return (
     <div className="bg-gray-800">
       <div className="card-container">
-        {Array.from(dataMap).reverse().map(([key, value]) => (
-          <div key={key} className="card">
-            <h2>{value.datetime}</h2>
-            <p>
-              <strong>Mood Rating:</strong>{" "}
-              {decrypt(value.moodrating, localStorage.getItem("pin"))}
-              {"/10"}
-            </p>
-            <p>
-              <strong>Mood Notes:</strong>{" "}
-              {decrypt(value.moodnotes, localStorage.getItem("pin"))}
-            </p>
-            <div>
-              <strong>Current Weather:</strong>
+        {Array.from(dataMap)
+          .reverse()
+          .map(([key, value]) => (
+            <div key={key} className="card">
+              <h2>{value.datetime}</h2>
               <p>
-                Temperature:{" "}
-                {Math.round(value.currentweather.currenttemp) + "°C"}
+                <strong>Mood Rating:</strong>{" "}
+                {decrypt(value.moodrating, localStorage.getItem("pin"))}
+                {"/10"}
               </p>
               <p>
-                Description: {value.currentweather.currentWeatherDescription}
+                <strong>Mood Notes:</strong>{" "}
+                {decrypt(value.moodnotes, localStorage.getItem("pin"))}
               </p>
+              <div>
+                <strong>Current Weather:</strong>
+                <p>
+                  Temperature:{" "}
+                  {Math.round(value.currentweather.currenttemp) + "°C"}
+                </p>
+                <p>
+                  Description: {value.currentweather.currentWeatherDescription}
+                </p>
+              </div>
+              <div>
+                <button
+                  onClick={() => onShare(value)}
+                  className="submit-button mt-3"
+                >
+                  Share
+                </button>
+              </div>
             </div>
-            <div>
-              <button
-                onClick={() => onShare(value)}
-                className="submit-button mt-3"
-              >
-                Share
-              </button>
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
